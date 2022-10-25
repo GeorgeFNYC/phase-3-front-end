db5381fd58c492a2745ee85b4df72a0022ad6fd7
George Fernandez <fernandezg704@gmail.com>
2022-10-25 14:11:39 -0400

    added stamps and the progress bar to the progress component

--- src/components/Progress.js
+++ src/components/Progress.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 import '../App.css'
+import bk from '../brooklynStamp.png'
+import man from '../manhattanStamp.png'
 
 function Progress() {
   return (
@@ -10,9 +13,25 @@ function Progress() {
             <div className='col-lg-12 insideBook'>
                 <div className='col-lg-6 leftContent'>
                     <h2>Progress</h2>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <br/>
+                    <h3>Brooklyn</h3>
+                        <ProgressBar variant='info' now={30}/>
+                    <br/>
+                    <br/>
+                    <h3>Manhattan</h3>
+                    <ProgressBar variant='info' now={100}/>
                 </div>
                 <div className='col-lg-6 rightContent'>
                     <h2>Stamps collected</h2>
+                    <br/>
+                    <br/>
+                    <img id='bkStamp' src={bk} alt='Brooklyn Stamp'/>
+                    <br/>
+                    <br/>
+                    <img id='manStamp' src={man} alt='Manhattan Stamp'/>
                 </div>
             </div>
         </div>
